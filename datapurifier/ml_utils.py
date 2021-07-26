49d90d165036fff89b38f5d740add645e2c0d173
Abhishek Gupta <abhig0209@gmail.com>
2021-07-26 07:16:52 +0530

    Mlpurifier completed

--- datapurifier/ml_utils.py
+++ datapurifier/ml_utils.py
@@ -1,26 +1,24 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-
-from IPython.display import display
-
 from termcolor import colored
+import matplotlib.pyplot as plt
+from IPython.display import display
 
 import warnings
 warnings.filterwarnings("ignore")
 
 def get_categorical_columns(df) -> list:
-        '''
-        Return list of categorical features
-        '''
-        cat_cols = df.select_dtypes(include="O").columns.tolist()
-        return cat_cols
+    '''Return list of categorical features
+    '''
+    
+    cat_cols = df.select_dtypes(include="O").columns.tolist()
+    return cat_cols
 
 def get_numerical_columns(df) -> list:
+    '''Return list of numerical features
     '''
-    Return list of numerical features
-    '''
+    
     numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     num_cols = df.select_dtypes(include=numerics).columns.tolist()
     return num_cols
@@ -41,8 +39,7 @@ def sample(df, num_or_rows: int = 10, all_columns: bool = True) -> None:
 
 
 def columns_type(df, verbose=False) -> tuple:
-    '''
-    return number of categorical and numerical columns
+    '''return number of categorical and numerical columns
     '''
     
     cat_columns = get_categorical_columns(df)
@@ -58,10 +55,10 @@ def columns_type(df, verbose=False) -> tuple:
          
     return (len(cat_columns), len(num_columns))
 
-def null_columns_percentage( df) -> pd.DataFrame:
+def null_columns_percentage(df) -> pd.DataFrame:
+    '''Prints Null Information of dataframe, i.e. only the number of rows having null values and their null percentage
     '''
-    Prints Null Information of dataframe, i.e. only the number of rows having null values and their null percentage
-    '''
+    
     print("\nNull Information of Dataframe: \n")
     null_df = pd.DataFrame(df.isnull().sum()).reset_index()
     null_df.columns = ["column_name", "null_rows"]
@@ -72,3 +69,98 @@ def null_columns_percentage( df) -> pd.DataFrame:
         f"\nThere are total {null_df.shape[0]} columns having null values out of {df.shape[1]} columns in dataframe\n", "red", attrs=["bold"]))
     display(null_df)
     return null_df
+
+def unique_df_percentage(df) -> pd.DataFrame:
+    """Returns dataframe containing (column name, count of total unique values per column, uniqueness percentage) 
+    """
+    
+    data = {"column": [], "unique_count": [], "unique_percent": []}
+    for col in df.columns:
+        data["column"].append(col)
+        data["unique_count"].append(df[col].nunique()) 
+        data["unique_percent"].append(data["unique_count"][-1]/ df.shape[0] *100)
+        
+    unique_df = pd.DataFrame(data)
+        
+    return unique_df
+
+"""##################################################################################################################################
+Drop functions
+##################################################################################################################################"""
+
+
+def drop_single_value_column(df):
+    """Drop columns having a single value or zero variance
+    """
+    counts = df.nunique()
+    # record columns to delete
+    to_del = [i for i,val in enumerate(counts) if val == 1]
+    if to_del:
+        print("Dropped columns containing single value: ", to_del)
+        df.drop(to_del, axis=1, inplace=True)
+    else:
+        print("No columns contains single value")
+    return df   
+
+def drop_column_based_on_uniqueness_threshold(df: pd.DataFrame, uniqueness_threshold: float = 0.95, exact: bool = False) -> pd.DataFrame:
+    '''Removes columns having unique value more than 95% (uniqueness_threshold), like: ID, serial_no, etc
+    
+    Parameters:
+        df (pd.DataFrame): input dataframe
+        uniqueness_threshold (float): value of uniqueness column precentage is between 0 and 100
+        exact (bool): deletes column which have exactly 'unique column precentage' equal to 'uniqueness_threshold'
+
+    Returns:
+        Pandas dataframe with columns deleted having unique value more than or equal to 'uniqueness_threshold'
+    '''
+    
+    to_del = []
+    for col in df.columns:
+        if exact:
+            if df[col].nunique()/df.shape[0] == uniqueness_threshold:
+                to_del.append(col)
+        else:
+            if df[col].nunique()/df.shape[0] >= uniqueness_threshold:
+                to_del.append(col)
+
+    if to_del:
+        if exact:
+            print(colored(
+            f"\nDroping columns having uniqueness_threshold={uniqueness_threshold}\n", "red", attrs=["bold"]))
+        else:
+            print(colored(
+                f"\nDroping columns having uniqueness_threshold={uniqueness_threshold} or greater'\n", "red", attrs=["bold"]))
+        df.drop(to_del, axis=1, inplace=True)
+        print("Dropped Columns are: ", to_del)
+    else:
+        if exact:
+            print(f"No columns found having uniqueness_threshold of={uniqueness_threshold} or greater")
+        else:
+            print(f"No columns found having uniqueness_threshold of={uniqueness_threshold}")
+    return df
+    
+def drop_null_rows(df):
+    """Drop all rows containing null values"""
+    return df.dropna().reset_index(drop=True)
+    
+
+def drop_null_col_based_on_percentage(df, threshold):
+    '''Drop columns below having null values below certain threshold,
+    and returns clean dataframe
+    '''
+    if threshold >= 0 and threshold <= 1:
+        return df.loc[:, df.isin([' ', 'NULL', 0, np.nan]).mean() < threshold]
+    raise Exception("Threshold should be between 0 and 1")
+
+
+def drop_duplicate_rows(df):
+    """Drop Duplicate Rows from dataset
+    """
+    print("Checking for duplicate rows...")
+    if df.duplicated().sum() == 0:
+        print("Dataframe contains no duplicate rows")
+    else:
+        initial_rows = df.shape[0]
+        df.drop_duplicates(inplace=True)
+        print(colored(f"Dropped {initial_rows - df.shape[0]} duplicate rows", "red", attrs=["bold"]))
+    return df
--- datapurifier/preprocessing/mlpurifier.py
+++ datapurifier/preprocessing/mlpurifier.py
@@ -4,53 +4,24 @@ from termcolor import colored
 
 from IPython.display import display
 
+from datapurifier import ml_utils
+
 
 
 class Mlpurifier:
     def __init__(self, df: pd.DataFrame):
         self.df = df.copy()
-
-    def null_rows(self):
-        """Drop all rows containing null values"""
-        self.df.dropna().reset_index()
-
-    def drop_null_col_based_on_percentage(self, threshold):
-        '''
-        Drop columns below having null values below certain threshold,
-        and returns clean dataframe
-        '''
-        if threshold >= 0 and threshold <= 1:
-            return self.df.loc[:, self.df.isin([' ', 'NULL', 0, np.nan]).mean() < threshold]
-        raise Exception("Threshold should be between 0 and 1")
-    
-    def drop_single_value_column(self):
-        """Drop columns having a single value or zero variance
-        """
-        pass
         
-    def drop_duplicate_rows(self):
-        """Drop Duplicate Rows from dataset
-        """
-        pass
-    
-    
-    def _remove_unique_columns(self, df, uniqueness_threshold: float = 0.8) -> list:
-        '''
-        Removes columns having unique value more than 80% (uniqueness_threshold), like: ID, serial_no, etc
-        '''
-        unique_val_cols = []
-        for col in df.columns:
-            if df[col].nunique()/df.shape[0] > uniqueness_threshold:
-                unique_val_cols.append(col)
+        # Drop Single value columns 
+        self.df = ml_utils.drop_single_value_column(self.df)
+        print()
+        
+        # Drop duplicate rows
+        self.df = ml_utils.drop_duplicate_rows(self.df)
+        print()
 
-        if unique_val_cols:
-            print(colored(
-                f"\nDroping columns having number of unique value more than {uniqueness_threshold*100}%\n", "red", attrs=["bold"]))
-            print("Droped Columns are: ", unique_val_cols)
-            print("-"*50, "\n")
-            return [True, unique_val_cols]
-        return [False, []]
 
 
 if __name__ == '__main__':
-    ac = Mlpurifier()
+    # mlp = Mlpurifier(df)
+    pass
