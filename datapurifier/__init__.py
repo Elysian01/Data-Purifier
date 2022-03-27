c1a3db1e03cd525458c630b6edb1eb0f04380061
Abhishek Gupta <abhig0209@gmail.com>
2022-03-27 20:17:13 +0530

    unicode requirements added

--- datapurifier/__init__.py
+++ datapurifier/__init__.py
@@ -19,7 +19,7 @@ from datapurifier.preprocessing.nlpurifier import NLAutoPurifier
 from .dataset import *
 from .report import MlReport
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 # from datapurifier.preprocessing.contractions import CONTRACTIONS
 
